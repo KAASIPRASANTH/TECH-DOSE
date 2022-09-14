4c263b257c2b742bbd6b514ae3266dd7adb35c9d
KAASIPRASANTH <90397202+KAASIPRASANTH@users.noreply.github.com>
2022-09-14 09:33:40 +0530

    Add files via upload

---
+++ InterviewQuestions/Prob37_MaximumSubarray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Prob37_MaximumSubarray {
+    public static int maxSubArray(int[] nums) {
+        int currSum = 0,maxSum = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            currSum += nums[i];
+            if(currSum<nums[i]){
+                currSum = nums[i];
+            }
+            if(maxSum<currSum){
+                maxSum = currSum;
+            }
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i=0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+        sc.close();
+        System.out.println(maxSubArray(nums));
+    }
+}
