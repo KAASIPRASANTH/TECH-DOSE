51dd2f4695a3fadc09d93f19a5f7a4e539986c6e
KAASIPRASANTH <90397202+KAASIPRASANTH@users.noreply.github.com>
2022-09-16 16:06:09 +0530

    Add files via upload

---
+++ KthLargestElemenInAnArray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class KthLargestElemenInAnArray{
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<k;i++){
+            pq.add(nums[i]);
+        }
+        for(int i=k;i<nums.length;i++){
+            if(nums[i]>pq.peek()){
+                pq.poll();
+                pq.add(nums[i]);
+            }
+        }
+        return pq.peek();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i=0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        sc.close();
+        System.out.println(findKthLargest(nums, k));
+    }
+}
