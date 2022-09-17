526fe2a287364483be96532d45becf26e79ea958
KAASIPRASANTH <90397202+KAASIPRASANTH@users.noreply.github.com>
2022-09-17 06:32:26 +0530

    Add files via upload

---
+++ InterviewQuestions/Prob69_RotateImage.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Prob69_RotateImage {
+    public static void rotate(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = i; j < col; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][col - j - 1];
+                matrix[i][col - j - 1] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        int[][] matrix = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+        rotate(matrix);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
