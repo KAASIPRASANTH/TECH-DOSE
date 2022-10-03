04d8efa13278cd9a311764f6676bd8b26acc76ff
KAASIPRASANTH <90397202+KAASIPRASANTH@users.noreply.github.com>
2022-10-03 17:55:49 +0530

    Add files via upload

---
+++ Arrays/LC/MajorityElement169.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MajorityElement169 {
+    public static int majorityElement(int[] nums) {
+        int majority = nums[0], count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (majority == nums[i]) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    majority = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return majority;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        sc.close();
+        System.out.println(majorityElement(nums));
+    }
+}
