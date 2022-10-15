6bc42478814ebd87e9cf493e6bcc662872658961
KAASIPRASANTH <90397202+KAASIPRASANTH@users.noreply.github.com>
2022-10-15 17:00:48 +0530

    Add files via upload

---
+++ LinkedList/LC/AddTwoNumbers2.java
@@ -0,0 +1,26 @@
+package LinkedList.LC;
+
+public class AddTwoNumbers2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(0);
+        ListNode temp = res;
+        int sum = 0;
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            temp.next = new ListNode(sum % 10);
+            sum /= 10;
+            temp = temp.next;
+        }
+        if (sum > 0) {
+            temp.next = new ListNode(sum);
+        }
+        return res.next;
+    }
+}
